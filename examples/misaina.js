59343d61697931270c72da012c56427f9b67e7f3
andrijdavid <andrijdavid@hotmail.com>
2017-03-03 23:32:23 +0300

    scaff

---
+++ examples/misaina.js
@@ -0,0 +1,32 @@
+const Mbot = require('../index');
+const bot = new Mbot({
+    "name": "Misaina",
+    "accessToken": "EAAIzFryBI4YBAA7Az6bCovVPQuwGp0fpkvsWznWlhy58n8da5sK0KHCcdgD3UjmzX1ni6O4otR7F4etRJ4hZC7QH79nfKXZC3IxfZBFhvmDOq5zRYOsKaaJjyjP5VsrFPZBLQMYnOA1FEyQnVNu73XTRBKPZBHnbWPciV6kkKKwZDZD",
+    "verifyToken": "zaertzvioUOHJBUUGLIUtoy321321e32r1t3e1t",
+    "appSecret": "32ee4c6e5a889406aa4096b6176d9aba",
+    "subdomain" : "misaina"
+});
+
+bot.on('message', (payload, chat) => {
+    const text = payload.message.text;
+    chat.say(`Echo: ${text}`);
+});
+
+
+bot.hear(/gif (.*)/i, (payload, chat, data) => {
+    const query = data.match[1];
+    chat.say('Searching for the perfect gif...');
+    fetch(GIPHY_URL + query)
+        .then(res => res.json())
+        .then(json => {
+            chat.say({
+                attachment: 'image',
+                url: json.data.image_url
+            }, {
+                typing: true
+            });
+        });
+});
+
+
+bot.start();
