8697807c64758f672744f65bad3c4de5a7971b36
andrijdavid <andrijdavid@hotmail.com>
2017-03-11 22:26:21 +0300

    scaff

--- modules/test.js
+++ modules/test.js
@@ -1,17 +1,30 @@
 'use strict';
 const Mbot = require('../index');
+const Message = require('../lib/Message');
+
 module.exports = (bot) => {
     bot.hear('test', (payload, chat) => {
         chat.say('Auto test');
-        let txtmsg = Mbot.TextMessageBuilder().setText('TextMessageBuilder test').build();
-        let audiomsg = Mbot.AudioMessageBuilder().setUrl(`https://${bot.name}.herokuapp.com/audios/perfect/mp3`).build();
-        let videomsg = Mbot.VideoMessageBuilder().setUrl(`https://${bot.name}.herokuapp.com/videos/vide.MP4`).build();
-        let imagemsg = Mbot.ImageMessageBuilder().setUrl(`https://${bot.name}.herokuapp.com/images/impossible.PNG`).build();
-        let filemsg = Mbot.FileMessageBuilder().setUrl(`https://${bot.name}.herokuapp.com/files/carnet.pdf`).build();
-        chat.say(txtmsg);
-        chat.say(audiomsg);
-        chat.say(videomsg);
-        chat.say(imagemsg);
-        chat.say(filemsg);
+        try{
+            let txtmsg = Mbot.TextMessageBuilder().setText('TextMessageBuilder test').build();
+            chat.say(txtmsg);
+
+        }
+        try{
+            let audiomsg = Mbot.AudioMessageBuilder().setUrl(`https://${bot.name}.herokuapp.com/audios/perfect/mp3`).build();
+            chat.say(audiomsg);
+        }
+        try{
+            let videomsg = Mbot.VideoMessageBuilder().setUrl(`https://${bot.name}.herokuapp.com/videos/vide.MP4`).build();
+            chat.say(videomsg);
+        }
+        try{
+            let imagemsg = Mbot.ImageMessageBuilder().setUrl(`https://${bot.name}.herokuapp.com/images/impossible.PNG`).build();
+            chat.say(imagemsg);
+        }
+        try{
+            let filemsg = Mbot.FileMessageBuilder().setUrl(`https://${bot.name}.herokuapp.com/files/carnet.pdf`).build();
+            chat.say(filemsg);
+        }
     });
 };
