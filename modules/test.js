16a9758dbb45778215e971facc012748b2e39a05
andrijdavid <andrijdavid@hotmail.com>
2017-03-11 22:32:35 +0300

    scaff

--- modules/test.js
+++ modules/test.js
@@ -5,26 +5,36 @@ const Message = require('../lib/Message');
 module.exports = (bot) => {
     bot.hear('test', (payload, chat) => {
         chat.say('Auto test');
-        try{
+        try {
             let txtmsg = Mbot.TextMessageBuilder().setText('TextMessageBuilder test').build();
             chat.say(txtmsg);
-
+        } catch (e) {
+            console.error(e);
         }
-        try{
+
+        try {
             let audiomsg = Mbot.AudioMessageBuilder().setUrl(`https://${bot.name}.herokuapp.com/audios/perfect/mp3`).build();
             chat.say(audiomsg);
+        } catch (e) {
+            console.error(e);
         }
-        try{
+        try {
             let videomsg = Mbot.VideoMessageBuilder().setUrl(`https://${bot.name}.herokuapp.com/videos/vide.MP4`).build();
             chat.say(videomsg);
+        } catch (e) {
+            console.error(e);
         }
-        try{
+        try {
             let imagemsg = Mbot.ImageMessageBuilder().setUrl(`https://${bot.name}.herokuapp.com/images/impossible.PNG`).build();
             chat.say(imagemsg);
+        } catch (e) {
+            console.error(e);
         }
-        try{
+        try {
             let filemsg = Mbot.FileMessageBuilder().setUrl(`https://${bot.name}.herokuapp.com/files/carnet.pdf`).build();
             chat.say(filemsg);
+        } catch (e) {
+            console.error(e);
         }
     });
 };
